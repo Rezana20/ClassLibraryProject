d019287bc6bcd2f46515d1783589163020f94cb2
Rezana <ganierezana8@gmail.com>
2019-09-25 17:58:07 +0200

    class library .net core example

---
+++ StringLibraryTest/TestStringLibrary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UtilityLibraries;
+
+namespace StringLibraryTest
+{
+    [TestClass]
+    public class TestStringLibrary
+    {
+        [TestMethod]
+        public void TestStartsWithUpper()
+        {
+
+            //Tests are expected to return true
+            string[] words = {"Alphabet", "Zebra", "ABC", "Αθήνα", "Москва"};
+
+            foreach (var word in words)
+            {
+                bool result = word.StartsWithUpper();
+                Assert.IsTrue(result,
+                    String.Format("Expected for '{0}': true; Actual: {1}",word, result));
+            }
+        }
+
+        [TestMethod]
+        public void TestDoesNotStartWithUpper()
+        {
+            string[] words = { "alphabet", "zebra", "abc", "αυτοκινητοβιομηχανία", "государство", "1234", ".", ";", " " };
+
+            foreach (var word in words)
+            {
+                bool result = word.StartsWithUpper();
+                Assert.IsFalse(result,String.Format("Expected for '{0}': false; Actual: {1}",word, result));
+            }
+
+        }
+
+        [TestMethod]
+        public void DirectCallWithNullOrEmpty()
+        {
+            string[] words = {string.Empty, null};
+
+            foreach (var word in words)
+            {
+                bool result = word.StartsWithUpper();
+                Assert.IsFalse(result, String.Format("Expected for '{0}': false; Actual: {1}",word == null ? "<null>" : word, result));
+
+            }
+
+
+        }
+    }
+}
